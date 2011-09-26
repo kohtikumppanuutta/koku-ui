5a6740b99b18e75621264568fd4d6f508ea6c539
Petri Mikkonen <petri.mikkonen@ixonos.com>
2011-09-26 13:08:06 +0000

    Added utils-class to retrieve UserInfo-object more easily

---
+++ portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/UserInfoUtils.java
@@ -0,0 +1,40 @@
+/**
+ * Utility/Helper class to get UserInfo-related data more easily.
+ * Notice: Use of this class requires use of UserInfoPortletFilter in portlet
+ */
+package fi.koku.portlet.filter.userinfo;
+
+import javax.portlet.PortletSession;
+import fi.koku.portlet.filter.userinfo.UserInfo;
+
+/**
+ * @author mikkope, tuomape
+ *
+ */
+public class UserInfoUtils {
+
+  /**
+   * Get user's PIC from session
+   * 
+   * @param session, is user's portletSession object
+   * @return PIC (HETU in this case) or empty string ("") if not found.
+   */
+  public static String getPicFromSession(PortletSession session) {
+    UserInfo info = UserInfoUtils.getUserInfoFromSession(session);
+    if (info == null) {
+      return "";
+    }
+    return info.getPic();
+  }
+
+  /**
+   * Get UserInfo object from portlet session
+   * 
+   * @param session, is user's portletSession object
+   * @return UserInfo-object filled with user data or null, if not found
+   */
+  public static UserInfo getUserInfoFromSession(PortletSession session) {
+    return (UserInfo) session.getAttribute(UserInfo.KEY_USER_INFO);
+  }
+  
+}
