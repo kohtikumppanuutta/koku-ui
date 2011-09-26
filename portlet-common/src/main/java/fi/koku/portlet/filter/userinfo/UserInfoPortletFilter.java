c63c6e0473945bcfc6c5af277af26f6a2353e3f6
Petri Mikkonen <petri.mikkonen@ixonos.com>
2011-09-26 10:35:44 +0000

    pom.xml -Deployed 0.0.5 version to Artifactory UserInfoPortletFilter.java -Changed filter "type" from post to pre-filter. -Changed filter to activate on both render and action phases -Added some comments to methods

--- portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/UserInfoPortletFilter.java
+++ portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/UserInfoPortletFilter.java
@@ -8,10 +8,14 @@ package fi.koku.portlet.filter.userinfo;
 
 import java.io.IOException;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
 import javax.portlet.PortletSession;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.portlet.filter.ActionFilter;
 import javax.portlet.filter.FilterChain;
 import javax.portlet.filter.FilterConfig;
 import javax.portlet.filter.RenderFilter;
@@ -26,39 +30,89 @@ import fi.koku.portlet.filter.userinfo.service.UserInfoService;
  * 
  */
 
-public class UserInfoPortletFilter implements RenderFilter {
+public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
 
   private static final Logger log = LoggerFactory.getLogger(UserInfoPortletFilter.class);
   private UserInfoService userInfoService = null;
-  
-  
+
+  /**
+   * Initialize portlet filter. portlet.xml:init-param named authImplClassName
+   * will be instantiated in this method. This impl class is responsible for
+   * getting userData with portalUserId.
+   */
   public void init(FilterConfig filterConfig) {
-    
-    //This just an example how to get information from portlet.xml
+
+    // This just an example how to get information from portlet.xml
     String implClassName = filterConfig.getInitParameter("authImplClassName");
     log.debug("Initializing portlet: authImplClassName=" + implClassName);
 
-    //Load configured class
+    // Load configured class
     try {
-      userInfoService = (UserInfoService)Class.forName(implClassName).newInstance();
+      userInfoService = (UserInfoService) this.getClass().getClassLoader().loadClass(implClassName).newInstance();
+
     } catch (InstantiationException e) {
-      log.error("Failed to Instantiate classname="+implClassName,e);
+      log.error("Failed to Instantiate classname=" + implClassName, e);
     } catch (IllegalAccessException e) {
-      log.error("Failed to Instantiate. Illegal Access on classname="+implClassName,e);
+      log.error("Failed to Instantiate. Illegal Access on classname=" + implClassName, e);
     } catch (ClassNotFoundException e) {
-      log.error("Class not found. Classname="+implClassName,e);
+      log.error("Class not found. Classname=" + implClassName, e);
     }
-    
-    
 
   }
 
+  /**
+   * RenderPhase pre-filter : Get userData with remoteId and add UserInfo-object
+   * to portlet session
+   */
   public void doFilter(RenderRequest request, RenderResponse response, FilterChain filterChain) {
-    log.debug("Entered UserInfo filter");
+    log.debug("Entered UserInfo filter on renderPhase");
 
-    //Allow filter chaining
-    doFilterChaining(request, response, filterChain);
+    addUserInfoToSession(request);
+
+    // Allow filter chaining
+    try {
+      filterChain.doFilter(request, response);
+    } catch (IOException e) {
+      log.error("Failed to doFilterChain", e);
+    } catch (PortletException e) {
+      log.error("Failed to doFilterChain", e);
+    }
+    log.debug("Finished renderPhase filterChaining");
+
+  }
+
+  /**
+   * ActionPhase pre-filter : Get userData with remoteId and add UserInfo-object
+   * to portlet session
+   */
+  public void doFilter(ActionRequest actReq, ActionResponse actRes, FilterChain filterChain) throws IOException,
+      PortletException {
+    log.debug("Entered UserInfo filter on actionPhase");
+
+    addUserInfoToSession(actReq);
     
+    // Allow filter chaining
+    try {
+      filterChain.doFilter(actReq, actRes);
+    } catch (IOException e) {
+      log.error("Failed to doFilterChain", e);
+    } catch (PortletException e) {
+      log.error("Failed to doFilterChain", e);
+    }
+    log.debug("Finished actionPhase filterChaining");
+
+  }
+
+  /**
+   * This method does the actual work.
+   * 
+   * After checking that user has remoteUserId (is signed in portal) this method
+   * gets userData from UserInfoService interface and adds it to UserInfo object
+   * which is stored into portlet session
+   * 
+   * @param request
+   */
+  private void addUserInfoToSession(PortletRequest request) {
     // Get portal user id
     String portalUserId = request.getRemoteUser();
     log.debug("req.getRemoteUser=" + portalUserId);
@@ -70,46 +124,36 @@ public class UserInfoPortletFilter implements RenderFilter {
       // Check if portlet-session exists already
       if (psession != null) {
         Object o = psession.getAttribute(UserInfo.KEY_USER_INFO);
-        
+
         // Check if UserInfo
         if (o != null && o instanceof UserInfo) {
           // Portal user has UserInfo object already in portletsession = OK
-          log.debug("psession.userInfo="+(UserInfo)o);//#TODO# Consider removing hetu from log
+          log.debug("psession.userInfo=" + (UserInfo) o);// #TODO# Consider
+                                                         // removing hetu from
+                                                         // log
         } else {
 
           // UserInfo does not exist yet
-          
+
           // #TODO# Here we have to remember that Loora and Kunpo has different
           // mechanisms after VETUMAIntegration. Now impl "Loora-case"
           UserInfo userInfo = userInfoService.getUserInfoById(portalUserId);
-          if(userInfo!=null){
+          if (userInfo != null) {
             psession.setAttribute(UserInfo.KEY_USER_INFO, userInfo);
-            log.debug("Following UserInfo-object is added to portlet sesssion : "+userInfo);
-          }else{
+            log.debug("Following UserInfo-object is added to portlet sesssion : " + userInfo);
+          } else {
             log.debug("userInfo=null and therefore not added to portletsession");
           }
-          
+
         }
       } else {
         log.debug("PortletSession=null : user probably has not logged in and this is probably the first request.");
       }
 
     } else {
-      //Not authenticated
+      // Not authenticated
       log.debug("Portal remoteUser not found so User has probably not logged in!");
     }
-
-  }
-
-  private void doFilterChaining(RenderRequest request, RenderResponse response, FilterChain filterChain) {
-    try {
-      filterChain.doFilter(request, response);
-    } catch (IOException e) {
-      log.error("Failed to doFilterChain", e);
-    } catch (PortletException e) {
-      log.error("Failed to doFilterChain", e);
-    }
-    log.debug("Finished filterChaining");
   }
 
   @Override
