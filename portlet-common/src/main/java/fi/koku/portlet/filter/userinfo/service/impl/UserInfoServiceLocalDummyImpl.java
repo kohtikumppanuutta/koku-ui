b583507ff6012ee20ece5381361aa50252dfb2f1
Petri Mikkonen <petri.mikkonen@ixonos.com>
2011-09-28 10:22:03 +0000

    Updated demo6-spesific user information

--- portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/service/impl/UserInfoServiceLocalDummyImpl.java
+++ portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/service/impl/UserInfoServiceLocalDummyImpl.java
@@ -15,7 +15,64 @@ public class UserInfoServiceLocalDummyImpl implements UserInfoService {
   public UserInfo getUserInfoById(String uid) {
     UserInfo u = new UserInfo();
 
-    if ("isa".equals(uid)) {
+   if ("kirsi.kuntalainen".equals(uid)) {
+    u.setUid("kirsi.kuntalainen");
+    u.setPic("222222-2222");
+    u.setFname("Kirsi");
+    u.setSname("Kuntalainen");
+  } else if ("kalle.kuntalainen".equals(uid)) {
+    u.setUid("kalle.kuntalainen");
+    u.setPic("111111-1111");
+    u.setFname("Kalle");
+    u.setSname("Kuntalainen");
+  } else if ("kaisa.kuntalainen".equals(uid)) {
+    u.setUid("kaisa.kuntalainen");
+    u.setPic("444444-4444");
+    u.setFname("Kaisa");
+    u.setSname("Kuntalainen");
+  } else if ("kauko.kuntalainen".equals(uid)) {
+    u.setUid("kauko.kuntalainen");
+    u.setPic("555555-5555");
+    u.setFname("Kauko");
+    u.setSname("Kuntalainen");
+  } else if ("keijo.kepuli".equals(uid)) {
+    u.setUid("keijo.kepuli");
+    u.setPic("131313-1313");
+    u.setFname("Keijo");
+    u.setSname("Kepuli");
+  } else if ("kerttu.kuntalainen".equals(uid)) {
+    u.setUid("kerttu.kuntalainen");
+    u.setPic("333333-3333");
+    u.setFname("Kerttu");
+    u.setSname("Kuntalainen");
+  } else if ("paivi.paivakoti".equals(uid)) {
+    u.setUid("paivi.paivakoti");
+    u.setPic("777777-7777");
+    u.setFname("Paivi");
+    u.setSname("Paivakoti");
+  } else if ("nelli.neuvola".equals(uid)) {
+    u.setUid("nelli.neuvola");
+    u.setPic("888888-8888");
+    u.setFname("Nelli");
+    u.setSname("Neuvola");
+  } else if ("veeti.virkamies".equals(uid)) {
+    u.setUid("veeti.virkamies");
+    u.setPic("999999-9999");
+    u.setFname("Veeti");
+    u.setSname("Virkamies");
+  } else if ("pertti.paakayttaja".equals(uid)) {
+    u.setUid("pertti.paakayttaja");
+    u.setPic("101010-1010");
+    u.setFname("Pertti");
+    u.setSname("Paakayttaja");
+  } else if ("piia.paakayttaja".equals(uid)) {
+    u.setUid("piia.paakayttaja");
+    u.setPic("121212-1212");
+    u.setFname("Piia");
+    u.setSname("Paakayttaja");
+  
+  //OLD USERS  
+  } else if ("isa".equals(uid)) {
       u.setUid("isa");
       u.setPic("111111-1111");
       u.setFname("Isa");
