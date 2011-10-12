52ecff4d5ffa7011a096fd5800aa96b7878b4c89
Mikko Hurula <mikko.hurula@ixonos.com>
2011-10-12 09:36:07 +0000

    missing file related to KOKU-596 added

---
+++ pyh/src/main/java/com/ixonos/koku/pyh/GuardianForChildNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ixonos.koku.pyh;
+
+public class GuardianForChildNotFoundException extends Exception {
+  
+  public GuardianForChildNotFoundException(String message) {
+    super(message);
+  }
+  
+}
