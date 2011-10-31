8cee934b4955e70734da1a92e5baaeabd6fd1455
marko asplund <marko.asplund@ixonos.com>
2011-10-31 10:22:45 +0000

    KOKU-656: add file header

--- pyh/src/main/java/com/ixonos/koku/pyh/model/Dependant.java
+++ pyh/src/main/java/com/ixonos/koku/pyh/model/Dependant.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2011 Ixonos Plc, Finland. All rights reserved.
+ * 
+ * You should have received a copy of the license text along with this program.
+ * If not, please contact the copyright holder (http://www.ixonos.com/).
+ * 
+ */
 package com.ixonos.koku.pyh.model;
 
 import fi.koku.services.entity.customer.v1.CustomerType;
