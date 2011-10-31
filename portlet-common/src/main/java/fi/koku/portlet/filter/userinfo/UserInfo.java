055d451c207b16f802e1260ddc90a561b042bfb6
marko asplund <marko.asplund@ixonos.com>
2011-10-31 10:26:11 +0000

    KOKU-656: add file header

--- portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/UserInfo.java
+++ portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/UserInfo.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2011 Ixonos Plc, Finland. All rights reserved.
+ * 
+ * You should have received a copy of the license text along with this program.
+ * If not, please contact the copyright holder (http://www.ixonos.com/).
+ * 
+ */
 /**
  * This class holds information about the user. The actual data is retrieved from external sources
  * in portlet filter
--- portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/UserInfoUtils.java
+++ portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/UserInfoUtils.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2011 Ixonos Plc, Finland. All rights reserved.
+ * 
+ * You should have received a copy of the license text along with this program.
+ * If not, please contact the copyright holder (http://www.ixonos.com/).
+ * 
+ */
 /**
  * Utility/Helper class to get UserInfo-related data more easily.
  * Notice: Use of this class requires use of UserInfoPortletFilter in portlet
