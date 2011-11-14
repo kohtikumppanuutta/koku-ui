7bb569f16110a870e8da573b64a67d1cfb63b85c
Petri Tuomaala <petri.tuomaala@ixonos.com>
2011-11-14 11:16:56 +0000

    KOKU-745

--- portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/UserInfoPortletFilter.java
+++ portlet-common/src/main/java/fi/koku/portlet/filter/userinfo/UserInfoPortletFilter.java
@@ -17,17 +17,25 @@ import java.io.IOException;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.EventRequest;
+import javax.portlet.EventResponse;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
+import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.PortletResponse;
 import javax.portlet.PortletSession;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 import javax.portlet.filter.ActionFilter;
+import javax.portlet.filter.EventFilter;
 import javax.portlet.filter.FilterChain;
 import javax.portlet.filter.FilterConfig;
 import javax.portlet.filter.RenderFilter;
+import javax.portlet.filter.ResourceFilter;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,10 +51,11 @@ import fi.koku.settings.KoKuPropertiesUtil;
  * 
  */
 
-public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
+public class UserInfoPortletFilter implements RenderFilter, ActionFilter, EventFilter, ResourceFilter {
 
   private static final Logger log = LoggerFactory.getLogger(UserInfoPortletFilter.class);
   private UserInfoService userInfoService = null;
+  private FilterConfig filterConfig;
 
   private AuthenticationServiceClient authServiceClient = null;
 
@@ -56,7 +65,7 @@ public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
   public void init(FilterConfig filterConfig) {
 
     log.debug("Initializing portlet: authImplClassName=" + UserInfoConstants.AUTH_IMPL_CLASS_NAME);
-
+    this.filterConfig = filterConfig;
     // Load configured class
     try {
 
@@ -64,22 +73,25 @@ public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
           .loadClass(UserInfoConstants.AUTH_IMPL_CLASS_NAME).newInstance();
 
       String context = KoKuPropertiesUtil.get("environment.name");
-      
+
       if ("kunpo".equals(context)) {
         authServiceClient = AuthenticationServiceClientImpl.instance();
       } else {
         authServiceClient = new AuthenticationServiceClient() {
-        // Dummy implementation for Loora. Should never be used.
+          // Dummy implementation for Loora. Should never be used.
           public boolean hasStrongAuthentication(PortletRequest request) {
             return false;
           }
+
           public VetumaUserInfo getVetumaUserInfo(PortletRequest request) {
             return null;
           }
+
           public String getAuthenticationURL(RenderResponse response, String id) {
             return null;
-          }};
-          
+          }
+        };
+
       }
 
     } catch (InstantiationException e) {
@@ -96,18 +108,21 @@ public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
    * RenderPhase pre-filter : Get userData with remoteId and add UserInfo-object
    * to portlet session
    */
-  public void doFilter(RenderRequest request, RenderResponse response, FilterChain filterChain) {
+  public void doFilter(RenderRequest request, RenderResponse response, FilterChain filterChain) throws IOException,
+  PortletException {
     log.debug("Entered UserInfo filter on renderPhase");
 
     addUserInfoToSession(request, response);
 
-    // Allow filter chaining
-    try {
-      filterChain.doFilter(request, response);
-    } catch (IOException e) {
-      log.error("Failed to doFilterChain", e);
-    } catch (PortletException e) {
-      log.error("Failed to doFilterChain", e);
+    if (isAuthorized(request, response)) {
+      // Allow filter chaining
+      try {
+        filterChain.doFilter(request, response);
+      } catch (IOException e) {
+        log.error("Failed to doFilterChain", e);
+      } catch (PortletException e) {
+        log.error("Failed to doFilterChain", e);
+      }
     }
     log.debug("Finished renderPhase filterChaining");
 
@@ -123,13 +138,15 @@ public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
 
     addUserInfoToSession(actReq, actRes);
 
-    // Allow filter chaining
-    try {
-      filterChain.doFilter(actReq, actRes);
-    } catch (IOException e) {
-      log.error("Failed to doFilterChain", e);
-    } catch (PortletException e) {
-      log.error("Failed to doFilterChain", e);
+    if (isAuthorized(actReq, actRes)) {
+      // Allow filter chaining
+      try {
+        filterChain.doFilter(actReq, actRes);
+      } catch (IOException e) {
+        log.error("Failed to doFilterChain", e);
+      } catch (PortletException e) {
+        log.error("Failed to doFilterChain", e);
+      }
     }
     log.debug("Finished actionPhase filterChaining");
 
@@ -145,29 +162,29 @@ public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
    * @param request
    */
   private void addUserInfoToSession(PortletRequest request, PortletResponse response) {
-    
+
     UserInfo userInfo = getUserInfo(request);
 
     if (userInfo == null) {
-      
+
       if (log.isDebugEnabled()) {
         log.debug("Couldn't fetch user info.");
       }
-      
+
       return;
     }
-    
+
     if (userInfo.isStrongAuthenticationEnabled() && !userInfo.hasStrongAuthentication()) {
-      
+
       if (log.isDebugEnabled()) {
         log.debug("User does not have strongly authenticated session.");
-      }      
-      
+      }
+
       setStrongAuthentication(request, response, userInfo);
     }
-    
+
     request.getPortletSession().setAttribute(UserInfo.KEY_USER_INFO, userInfo);
-    
+
   }
 
   private void setStrongAuthentication(PortletRequest request, PortletResponse response, UserInfo userInfo) {
@@ -180,19 +197,20 @@ public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
   }
 
   private void setAuthenticationURL(PortletRequest request, PortletResponse response, UserInfo userInfo) {
-    
+
     if (!(response instanceof RenderResponse)) {
       log.warn("PortletResponse is not instance of RenderResponse. Could not create authentication url.");
       return;
     }
-    
+
     if (request.getPortletSession().getAttribute(UserInfo.KEY_VETUMA_AUTHENTICATION_URL) == null) {
-      
-      String url = authServiceClient.getAuthenticationURL((RenderResponse) response, userInfo.getUid()); 
-      
-      request.getPortletSession().setAttribute(UserInfo.KEY_VETUMA_AUTHENTICATION_URL,
-          url, PortletSession.PORTLET_SCOPE);
-      
+
+      String url = authServiceClient.getAuthenticationURL((RenderResponse) response, userInfo.getUid());
+
+      request.getPortletSession().setAttribute(UserInfo.KEY_VETUMA_AUTHENTICATION_URL, url,
+          
+          PortletSession.PORTLET_SCOPE);
+
       if (log.isDebugEnabled()) {
         log.debug("Set authentication url to " + url);
       }
@@ -218,18 +236,18 @@ public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
   }
 
   private UserInfo getUserInfo(PortletRequest request) {
-  // Fetch userinfo from session or from remote service
-    
-    UserInfo ui = (UserInfo)request.getPortletSession(true).getAttribute(UserInfo.KEY_USER_INFO);
-    
+    // Fetch userinfo from session or from remote service
+
+    UserInfo ui = (UserInfo) request.getPortletSession(true).getAttribute(UserInfo.KEY_USER_INFO);
+
     if (ui == null) {
-      
+
       String uid = request.getRemoteUser();
-      
+
       if (uid != null) {
 
         ui = userInfoService.getUserInfoById(uid);
-        
+
         if (log.isDebugEnabled()) {
           log.debug("Fetched userinfo " + ui + " from remote service.");
         }
@@ -245,13 +263,97 @@ public class UserInfoPortletFilter implements RenderFilter, ActionFilter {
         log.debug("Fetched userinfo " + ui + " from session.");
       }
     }
-    
+
     return ui;
   }
 
   @Override
   public void destroy() {
 
+  }
+
+  /**
+   * ResourcePhase pre-filter : Get userData with remoteId and add UserInfo-object
+   * to portlet session
+   */
+  public void doFilter(ResourceRequest request, ResourceResponse response, FilterChain chain) throws IOException,
+      PortletException {
+    log.debug("Entered UserInfo filter on resourcePhase");
+
+    addUserInfoToSession(request, response);
+
+    if (isAuthorized(request, response)) {
+      // Allow filter chaining
+      try {
+        chain.doFilter(request, response);
+      } catch (IOException e) {
+        log.error("Failed to doFilterChain", e);
+      } catch (PortletException e) {
+        log.error("Failed to doFilterChain", e);
+      }
+    }
+    log.debug("Finished renderPhase resourcePhase");
+
+  }
+
+  /**
+   * EventPhase pre-filter : Get userData with remoteId and add UserInfo-object
+   * to portlet session
+   */
+  public void doFilter(EventRequest request, EventResponse response, FilterChain chain) throws IOException,
+      PortletException {
+    log.debug("Entered UserInfo filter on eventPhase");
+
+    addUserInfoToSession(request, response);
+
+    if (isAuthorized(request, response)) {
+      // Allow filter chaining
+      try {
+        chain.doFilter(request, response);
+      } catch (IOException e) {
+        log.error("Failed to doFilterChain", e);
+      } catch (PortletException e) {
+        log.error("Failed to doFilterChain", e);
+      }
+    }
+    log.debug("Finished eventPhase filterChaining");
+
+  }
+
+  private boolean isLoggedIn(UserInfo ui) {
+
+    if (ui == null) {
+      return false;
+    }
+
+    if (ui.isStrongAuthenticationEnabled()) {
+      return ui.hasStrongAuthentication();
+    }
+    return StringUtils.isNotEmpty(ui.getPic());
+  }
+
+  private boolean isAuthorized(PortletRequest request, PortletResponse response) throws IOException,
+  PortletException {
+    UserInfo ui = getUserInfo(request);
+    if (!isLoggedIn(ui)) {
+   
+      PortletRequestDispatcher prd = filterConfig.getPortletContext().getRequestDispatcher(
+          "/WEB-INF/jsp/authenticate.jsp");
+
+      if ( ui != null && ui.isStrongAuthenticationEnabled() ) {
+        String authenticationURL = (String)request.getPortletSession().getAttribute(UserInfo.KEY_VETUMA_AUTHENTICATION_URL);
+        
+        if ( authenticationURL == null ) {
+          authenticationURL = "";
+        }
+        
+        request.setAttribute("authenticationURL", authenticationURL );
+      }
+      
+      prd.include(request, response);
+      return false;
+    }
+    return true;
   }
 
 }
