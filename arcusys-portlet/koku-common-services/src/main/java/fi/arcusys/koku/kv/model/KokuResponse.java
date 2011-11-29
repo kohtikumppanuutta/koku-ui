0fb61b8940404c696142ea0501f5f0e3408b340b
Toni Turunen <toni.turunen@arcusys.fi>
2011-11-29 12:04:32 +0000

    Issue KOKU-797 - Pyynnöt - huoltajan vastauslomakkeen Lisätiedot-kentän tekstiä ei löydy vastatuista pyynnöistä
    * Added comments to Loora side

--- arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/kv/model/KokuResponse.java
+++ arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/kv/model/KokuResponse.java
@@ -7,6 +7,7 @@ public class KokuResponse {
 	
 	private List<KokuAnswer> answers;
 	private String name;
+	private String comment;
 	
 	
 	public KokuResponse() {
@@ -18,6 +19,7 @@ public class KokuResponse {
 			return;
 		}
 		name = response.getName();
+		comment = response.getComment();
 		answers = new ArrayList<KokuAnswer>();
 		if (response.getAnswers() != null) {
 			for (fi.arcusys.koku.kv.requestservice.Answer answer : response.getAnswers()) {
@@ -38,21 +40,29 @@ public class KokuResponse {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	public final String getComment() {
+		return comment;
+	}
+	public final void setComment(String comment) {
+		this.comment = comment;
+	}
+
 	@Override
 	public String toString() {
-		return "KokuResponse [answers=" + answers + ", name=" + name + "]";
+		return "KokuResponse [answers=" + answers + ", name=" + name
+				+ ", comment=" + comment + "]";
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((answers == null) ? 0 : answers.hashCode());
+		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
@@ -66,17 +76,28 @@ public class KokuResponse {
 		}
 		KokuResponse other = (KokuResponse) obj;
 		if (answers == null) {
-			if (other.answers != null)
+			if (other.answers != null) {
 				return false;
+			}
 		} else if (!answers.equals(other.answers)) {
 			return false;
 		}
-		if (name == null) {
-			if (other.name != null)
+		if (comment == null) {
+			if (other.comment != null) {
 				return false;
+			}
+		} else if (!comment.equals(other.comment)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
 		} else if (!name.equals(other.name)) {
 			return false;
 		}
 		return true;
 	}
+
+	
 }
