3a46d1940a54c49c92fb5172a45a2d5bcabd3aab
Toni Turunen <toni.turunen@arcusys.fi>
2012-02-21 12:58:54 +0000

    Issue KOKU-1182 - Näytä käyttäjänimen sijasta etu- ja sukunimi.
    * Appointments fixed

--- arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/av/CitizenAppointment.java
+++ arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/av/CitizenAppointment.java
@@ -10,11 +10,8 @@ import fi.arcusys.koku.users.KokuUser;
 public class CitizenAppointment extends KokuAppointment{
 
 	private Slot slot;
-	private String replier;
 	private KokuUser replierUser;
 	private String replierComment;
-	private String targetPersonUid;
-    private String targetPersonDisplayName;
 	private KokuUser targetPersonUser;
 
 	
@@ -46,40 +43,8 @@ public class CitizenAppointment extends KokuAppointment{
 		this.replierUser = replierUser;
 	}
 
-	/**
-     * @return the targetPersonUid
-     */
-    public String getTargetPersonUid() {
-        return targetPersonUid;
-    }
-
-    /**
-     * @param targetPersonUid the targetPersonUid to set
-     */
-    public void setTargetPersonUid(String targetPersonUid) {
-        this.targetPersonUid = targetPersonUid;
-    }
-
-    /**
-     * @return the targetPersonDisplayName
-     */
-    public String getTargetPersonDisplayName() {
-        return targetPersonDisplayName;
-    }
-
-    /**
-     * @param targetPersonDisplayName the targetPersonDisplayName to set
-     */
-    public void setTargetPersonDisplayName(String targetPersonDisplayName) {
-        this.targetPersonDisplayName = targetPersonDisplayName;
-    }
-
     public Slot getSlot() {
 		return slot;
-	}
-	
-	public String getReplier() {
-		return replier;
 	}
 	
 	public String getReplierComment() {
@@ -89,12 +54,85 @@ public class CitizenAppointment extends KokuAppointment{
 	public void setSlot(Slot slot) {
 		this.slot = slot;
 	}
-	
-	public void setReplier(String replier) {
-		this.replier = replier;
-	}
-	
+		
 	public void setReplierComment(String replierComment) {
 		this.replierComment = replierComment;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ ((replierComment == null) ? 0 : replierComment.hashCode());
+		result = prime * result
+				+ ((replierUser == null) ? 0 : replierUser.hashCode());
+		result = prime * result + ((slot == null) ? 0 : slot.hashCode());
+		result = prime
+				* result
+				+ ((targetPersonUser == null) ? 0 : targetPersonUser.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		CitizenAppointment other = (CitizenAppointment) obj;
+		if (replierComment == null) {
+			if (other.replierComment != null) {
+				return false;
+			}
+		} else if (!replierComment.equals(other.replierComment)) {
+			return false;
+		}
+		if (replierUser == null) {
+			if (other.replierUser != null) {
+				return false;
+			}
+		} else if (!replierUser.equals(other.replierUser)) {
+			return false;
+		}
+		if (slot == null) {
+			if (other.slot != null) {
+				return false;
+			}
+		} else if (!slot.equals(other.slot)) {
+			return false;
+		}
+		if (targetPersonUser == null) {
+			if (other.targetPersonUser != null) {
+				return false;
+			}
+		} else if (!targetPersonUser.equals(other.targetPersonUser)) {
+			return false;
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "CitizenAppointment [slot=" + slot + ", replierUser="
+				+ replierUser + ", replierComment=" + replierComment
+				+ ", targetPersonUser=" + targetPersonUser + "]";
+	}
+	
+	
+	
 }
--- arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/av/Slot.java
+++ arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/av/Slot.java
@@ -1,5 +1,10 @@
 package fi.arcusys.koku.av;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fi.arcusys.koku.users.KokuUser;
+
 /**
  * Slot data model
  * @author Jinhua Chen
@@ -15,8 +20,8 @@ public class Slot {
     private String location;
     private String comment;
     private boolean approved;
-    private String targetPerson;
-    private String recipients;
+    private KokuUser targetPersonUser;
+    private final List<KokuUser> recipientUsers = new ArrayList<KokuUser>();;
     
     
     /* getters */
@@ -52,13 +57,6 @@ public class Slot {
     	return approved;
     }
     
-    public String getTargetPerson() {
-    	return targetPerson;
-    }
-    
-    public String getRecipients() {
-    	return recipients;
-    }
     /* setters */
     public void setAppointmentId(long appointmentId) {
     	this.appointmentId = appointmentId;
@@ -91,12 +89,144 @@ public class Slot {
     public void setApproved(boolean approved) {
     	this.approved = approved;
     }
+
+	/**
+	 * @return the targetPersonUser
+	 */
+	public final KokuUser getTargetPersonUser() {
+		return targetPersonUser;
+	}
+
+	/**
+	 * @param targetPersonUser the targetPersonUser to set
+	 */
+	public final void setTargetPersonUser(KokuUser targetPersonUser) {
+		this.targetPersonUser = targetPersonUser;
+	}
+
+	/**
+	 * @return the recipientUsers
+	 */
+	public final List<KokuUser> getRecipientUsers() {
+		return recipientUsers;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ (int) (appointmentId ^ (appointmentId >>> 32));
+		result = prime * result + (approved ? 1231 : 1237);
+		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((endTime == null) ? 0 : endTime.hashCode());
+		result = prime * result
+				+ ((location == null) ? 0 : location.hashCode());
+		result = prime * result
+				+ ((recipientUsers == null) ? 0 : recipientUsers.hashCode());
+		result = prime * result + slotNumber;
+		result = prime * result
+				+ ((startTime == null) ? 0 : startTime.hashCode());
+		result = prime
+				* result
+				+ ((targetPersonUser == null) ? 0 : targetPersonUser.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Slot other = (Slot) obj;
+		if (appointmentId != other.appointmentId) {
+			return false;
+		}
+		if (approved != other.approved) {
+			return false;
+		}
+		if (comment == null) {
+			if (other.comment != null) {
+				return false;
+			}
+		} else if (!comment.equals(other.comment)) {
+			return false;
+		}
+		if (date == null) {
+			if (other.date != null) {
+				return false;
+			}
+		} else if (!date.equals(other.date)) {
+			return false;
+		}
+		if (endTime == null) {
+			if (other.endTime != null) {
+				return false;
+			}
+		} else if (!endTime.equals(other.endTime)) {
+			return false;
+		}
+		if (location == null) {
+			if (other.location != null) {
+				return false;
+			}
+		} else if (!location.equals(other.location)) {
+			return false;
+		}
+		if (recipientUsers == null) {
+			if (other.recipientUsers != null) {
+				return false;
+			}
+		} else if (!recipientUsers.equals(other.recipientUsers)) {
+			return false;
+		}
+		if (slotNumber != other.slotNumber) {
+			return false;
+		}
+		if (startTime == null) {
+			if (other.startTime != null) {
+				return false;
+			}
+		} else if (!startTime.equals(other.startTime)) {
+			return false;
+		}
+		if (targetPersonUser == null) {
+			if (other.targetPersonUser != null) {
+				return false;
+			}
+		} else if (!targetPersonUser.equals(other.targetPersonUser)) {
+			return false;
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Slot [appointmentId=" + appointmentId + ", slotNumber="
+				+ slotNumber + ", date=" + date + ", startTime=" + startTime
+				+ ", endTime=" + endTime + ", location=" + location
+				+ ", comment=" + comment + ", approved=" + approved
+				+ ", targetPersonUser=" + targetPersonUser
+				+ ", recipientUsers=" + recipientUsers + "]";
+	}
+
+	
     
-    public void setTargetPerson(String targetPerson) {
-    	this.targetPerson = targetPerson;
-    }
-    
-    public void setRecipients(String recipients) {
-    	this.recipients = recipients;
-    }
+
 }
