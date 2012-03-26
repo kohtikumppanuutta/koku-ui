d45c7a0ca65b928fab1c96d74819fff5ed361828
Toni Turunen <toni.turunen@arcusys.fi>
2012-03-26 12:03:59 +0000

    Issue KOKU-1210 - Ajanvarauksessa tapaamisten ajat muuttuvat
    * Times will be now converted to GMT

--- arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/util/MessageUtil.java
+++ arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/util/MessageUtil.java
@@ -128,10 +128,9 @@ public final class MessageUtil {
 		if(xmlGregorianCalendar != null ) {
 			Calendar cal = xmlGregorianCalendar.toGregorianCalendar();
 			SimpleDateFormat dateformat = new SimpleDateFormat(formatString);
-			dateformat.setTimeZone(TimeZone.getTimeZone("GMT+0:00"));
+			dateformat.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
 			// dateformat.setTimeZone(TimeZone.getTimeZone("GMT+3:00"));
- 			// dateformat.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
-			return dateformat.format(cal.getTime());
+ 			return dateformat.format(cal.getTime());
 		} else {
 			return "";
 		}
