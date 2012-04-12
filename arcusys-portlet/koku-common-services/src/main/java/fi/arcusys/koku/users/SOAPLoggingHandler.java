9fe7ca758a3027fa45c96486e01eb0cf4e29bf6b
Toni Turunen <toni.turunen@arcusys.fi>
2012-04-12 15:33:03 +0300

    Issue KOKU-1258 - WS serviceiden luonti portletti puolella kestää liian kauan
    * Changed code how to load WebServices.

---
+++ arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/users/SOAPLoggingHandler.java
@@ -0,0 +1,103 @@
+package fi.arcusys.koku.users;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPException;
+import javax.xml.ws.Binding;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.Handler;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Logs SOAP in/out messages.
+ * @author mlind
+ *
+ */
+public class SOAPLoggingHandler implements SOAPHandler<SOAPMessageContext> {
+	private static final Logger LOG = Logger.getLogger(SOAPLoggingHandler.class);		
+
+	// private PrintStream out = ;
+
+	@Override
+	public final Set<QName> getHeaders() {
+		return null;
+	}
+
+	@Override
+	public final boolean handleMessage(SOAPMessageContext smc) {
+//		logToSystemOut(smc);
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			smc.getMessage().writeTo(out);
+			LOG.info(out.toString());
+		} catch (SOAPException e) {
+			LOG.error(e);
+			e.printStackTrace();
+		} catch (IOException ioe) {
+			LOG.error(ioe);
+			ioe.printStackTrace();
+		}
+		LOG.info("SOAP - '" + out.toString()+"'");
+		return true;
+	}
+
+	@Override
+	public final boolean handleFault(SOAPMessageContext smc) {
+		// logToSystemOut(smc);
+		return true;
+	}
+
+	@Override
+	public void close(MessageContext messageContext) {
+	}
+	
+//	private void logToSystemOut(SOAPMessageContext smc) {
+//		LOG.info("LOG SOAP ?!");
+//
+//		Boolean outboundProperty = (Boolean) smc.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+//		
+//		if (outboundProperty.booleanValue()) {
+//			out.println("\n["+new Date()+"] Outbound message:");
+//		} else {
+//			out.println("\n["+new Date()+"] Inbound message:");
+//		}
+//
+//		SOAPMessage message = smc.getMessage();
+//		try {
+//			message.writeTo(out);
+//			LOG.
+//			out.println(""); // just to add a newline
+//			LOG.error("SOAP msg - '" + out.toString() + "'");
+//			
+//		} catch (Exception e) {
+//			LOG.error("Exception in handler: "+ e);
+//			out.println("Exception in handler: " + e);
+//		}
+//	}
+	
+	@SuppressWarnings("rawtypes")
+	public static void addSOAPLogger(BindingProvider bp) {
+		LOG.info("Add SOAP logger");
+
+		//add logging
+		Binding binding = bp.getBinding();
+		List<Handler> handlerList = binding.getHandlerChain();
+		if (null == handlerList) {
+			handlerList = new ArrayList<Handler>();
+		}			
+		handlerList.add(new SOAPLoggingHandler());
+		binding.setHandlerChain(handlerList);
+	}
+
+}
